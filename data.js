30826b2016249cc6a45946f73b3e9ba4f9f22395
Tommc <blenderzen@hotmail.com>
2021-02-12 15:28:22 +0000

    first commit

---
+++ data.js
@@ -0,0 +1,278 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bq",
+      "name": "BQ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6644045407441439,
+          "pitch": 0.031000839979421713,
+          "rotation": 0,
+          "target": "1-johns-bridge"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.27822935252275016,
+          "pitch": -0.2674278646416859,
+          "title": "<p style=\"margin-bottom: 0cm; font-weight: normal; line-height: 100%\">\n<i><u>Welcome &amp; Introduction</u></i></p>",
+          "text": "<p style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 100%;\">&nbsp;This tour explores the famous black marble of Kilkenny. We start at\nthe old Quarry just outside the city. The tour then takes you to the\nold marble works on the banks of the Nore 5 miles from the city where\ninventor and entrepreneur William Colles was harnessing this great\npower of the river Nore.</p>"
+        },
+        {
+          "yaw": 0.7218229055311411,
+          "pitch": -0.25082348250089836,
+          "title": "<p style=\"margin-bottom: 0cm; font-weight: normal; line-height: 100%\">\n<i><u>Location Quarry Roadway</u></i></p>",
+          "text": "<p style=\"margin-bottom: 0cm; line-height: 100%\">\nHere\nin 1730\nWilliam Colles took\na perpetual\nlease\non the\nBlack Marble Quarry in Kilkenny. Just\nnorth of the city walls is the black marble quarry that gives the\nmarble city its name. Use the Arrow icon and move onto the section\nclosest to the river.&nbsp;&nbsp;</p>"
+        }
+      ]
+    },
+    {
+      "id": "1-johns-bridge",
+      "name": "John's Bridge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.794931134458043,
+          "pitch": -0.1436336461424137,
+          "rotation": 0,
+          "target": "2-sr"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.1455239569324167,
+          "pitch": -0.2577877186847388,
+          "title": "<p style=\"margin-bottom: 0cm; font-weight: normal; line-height: 100%\">\n<i><u>Location John’s Bridge</u></i></p>",
+          "text": "<p style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 100%;\">&nbsp;Raw blocks that were extracted from the Black quarry were then\ntransported from the quarry and over the Bridge here in the heart of\nKilkenny City. Previous bridges have been sweep away in floods and\nthe current bridge was rebuilt in?&nbsp; &nbsp;</p>"
+        }
+      ]
+    },
+    {
+      "id": "2-sr",
+      "name": "SR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.5249413365613744,
+        "pitch": 0.024193409348033157,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3362131526386367,
+          "pitch": -0.016550030065353383,
+          "rotation": 0,
+          "target": "3-cg_weighthouse"
+        },
+        {
+          "yaw": -0.30406597251923273,
+          "pitch": 0.1036539961333034,
+          "rotation": 0,
+          "target": "4-crane"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.36976705603027504,
+          "pitch": 0.04917309469662001,
+          "title": "<p style=\"margin-bottom: 0cm; font-weight: normal; line-height: 100%\">\n<i><u>Location Gates to Mills</u></i></p>",
+          "text": "<p style=\"margin-bottom: 0cm; line-height: 100%\">\nAlong\nwith the Black Quarry the lease\nincluded\ntwo\nexisting\nmills\nhere\nin\nHighrath Maddoxtown. \n</p>\n<p style=\"margin-bottom: 0cm; line-height: 100%\">The\nblack marble quarry &amp; Mills in Maddoxtown were in operation by\nthe Colles family for over 200 years and produced some 100 ton of the\nfamous black marble each year for export.</p>"
+        },
+        {
+          "yaw": 0.8650529964936471,
+          "pitch": -0.33576198722064454,
+          "title": "weight house",
+          "text": "CG Representation of the weight house used by the Marble Mills"
+        }
+      ]
+    },
+    {
+      "id": "3-cg_weighthouse",
+      "name": "CG_WeightHouse",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 2.7888165437824792,
+        "pitch": 0.053733169187687224,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-crane",
+      "name": "Crane",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0.2250018078608509,
+        "pitch": -0.003722062976594387,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.741582523802153,
+          "pitch": -0.3307368834402382,
+          "rotation": 0,
+          "target": "5-gy"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.816494013987036,
+          "pitch": -0.3532109771085654,
+          "title": "<p style=\"margin-bottom: 0cm; font-weight: normal; line-height: 100%\">\n<i><u>Location The Crane</u></i></p>",
+          "text": "<p style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 100%;\">&nbsp;The first piece of machinery inside the mill site was a large quarry\ncrane. This was used used to load and unload the heavy cargo coming\nin from the quarries and also the finished products leaving the\nworks.</p>"
+        }
+      ]
+    },
+    {
+      "id": "5-gy",
+      "name": "GY",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.20140745396812854,
+        "pitch": 0.12468103192212432,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.11785261931586177,
+          "pitch": -0.19284083959306386,
+          "title": "Location Graveyard",
+          "text": "12th Century Font"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
